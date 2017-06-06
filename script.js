64ba2515d233b80e469b1588bb5a124575504ebf
JCamp-12 <username@users.noreply.github.com>
2017-06-05 21:51:16 -0400

    Fixed Homework

--- script.js
+++ script.js
@@ -61,13 +61,12 @@ Hint: Use your last three loops and if/then/else statements. You can learn about
 */
 for(i = 1; i <= 100; i ++ )
 {
-    
-    if (i % 3 == 0)
-        document.write("Fizz" + "<br>");
+    if (i % 15 == 0)
+        document.write("FizzBuzz" + "<br>");
     else if (i % 5 == 0)
         document.write("Buzz" + "<br>");
-    else if (i % 15 == 0)
-        document.write("FizzBuzz" + "<br>");
+    else if (i % 3 == 0)
+        document.write("Fizz" + "<br>");
     else
         document.write(i + "<br>");
 }
